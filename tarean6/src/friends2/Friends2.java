3c142ceafabdd9936923eaac5843f122b1408a39
RicaroLandazuriLeyton <rlandazuri@unal.edu.co>
2025-03-03 16:29:41 -0500

    Add files via upload

---
+++ tarean6/src/friends2/Friends2.java
@@ -0,0 +1,11 @@
+package friends2;
+
+public class Friends2 {
+
+    public static void main(String[] args) {
+        MainForm frm = new MainForm();
+        frm.setVisible(true);
+        
+    }
+    
+}
